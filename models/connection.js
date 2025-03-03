084b2c45fdcd9c972c4ae7974d002c2787484ee9
Emile Karacostas <emile.karacostas@proton.me>
2025-03-03 09:47:09 +0100

    first commit

---
+++ models/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.CONNECTION_STRING;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(connectionString, { connectTimeoutMS: 2000 });
+    console.log('Database connected');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connectDB();
