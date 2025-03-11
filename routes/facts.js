ac1fc38f9e2adcb8a4f4fd86ae96c00b4ef4f1f6
matthieup-git <matthieu.posnic@gmail.com>
2025-03-11 17:55:23 +0100

    push

--- routes/facts.js
+++ routes/facts.js
@@ -7,18 +7,19 @@ const Fact = require("../models/facts");
 router.get('/', async (req, res) => {
     try {
 
-        const data = await Fact.find().select("-_id").sort({lastDisplayed : +1}).limit(1)
+        // requête pour afficher la dernière fact avec la date la plus lointaine
+        // const latestFact = await Fact.findOne().select("-_id").sort({lastDisplayed : +1})
 
-        // console.log("data", data[0])
+        const randomFact = await Fact.find().select("-_id")
 
-        // const factsLength = data.length
-        // const number = Math.floor(Math.random() * factsLength)
+        const factsLength = randomFact.length
+        const number = Math.floor(Math.random() * factsLength)
 
-        // if (data.length === 0 || factsLength < number ) {
-        //     return res.json({ result: false, error: "no facts found" })
-        // }
+        if (randomFact.length === 0 || factsLength < number ) {
+            return res.json({ result: false, error: "no facts found" })
+        }
         
-        res.json({ result: true, data: data[0] })
+        res.json({ result: true, data: randomFact[number] })
 
     } catch (error) {
         res.status(500).json({ result: false, error: error.message });
